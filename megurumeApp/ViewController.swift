f979faf31e2a24b8f366e76d1e75296519b1e1c6
mychair.kke@gmail.com <mychair.kke@gmail.com>
2019-03-20 15:56:50 +0900

    first commit

---
+++ megurumeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  megurumeApp
+//
+//  Created by RIVER on 2019/03/20.
+//  Copyright © 2019 kwtkMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
